61cef3512dd7b9d0c437418fc6783645f58f13ef
Josh Hendershot <hendershotjoshr@gmail.com>
2021-11-05 23:34:37 -0400

    added basic force physics

--- traffic_sim/traffic_sim.py
+++ traffic_sim/traffic_sim.py
@@ -1,29 +1,87 @@
+from math import radians
 import pygame
 from pygame import draw, Surface
 from random import randint
-from interfaces import TickableInterface, DrawableInterface
+
+from vector._backends.object_ import VectorObject2D
+from interfaces.interfaces import TickableInterface, DrawableInterface
+import vector
+import numpy as np
+
+
+def normalize(v: vector.VectorObject) -> vector.VectorObject:
+    normalized_v = v / np.sqrt(np.sum(v ** 2))
+    return normalized_v
 
 
 class Orb(DrawableInterface, TickableInterface):
     COLOR = (255, 255, 255)
-    pos = (0, 0)
-    size = 0
+    TARGET_COLOR = (50, 168, 82)
 
-    def init(self, pos: tuple, size: int):
+    pos = vector.obj(x=0, y=0)
+    velocity = vector.obj(x=0, y=0)
+    mass = 1
+    top_speed = 10
+    net_force = vector.obj(x=0, y=0)
+
+    size = vector.obj(x=10, y=10)
+    target = vector.obj(x=200, y=200)
+    min_speed = 0.1
+    max_force = 1
+    drag = 0.1
+
+    def init(
+        self,
+        pos: vector.VectorObject2D,
+        size: int,
+        target: vector.VectorObject2D = None,
+    ):
         self.pos = pos
-        self.size = size
+        self.size = vector.obj(x=size, y=size)
+        if target != None:
+            self.target = target
+
+    def set_target(self, target: VectorObject2D = None):
+        if target == None:
+            target = vector.obj(x=randint(0, 700), y=randint(0, 500))
+        self.target = target
+
+    def is_at_target(self) -> bool:
+        return abs(self.pos.subtract(self.target)) < self.top_speed
+
+    def add_force(self, force: VectorObject2D):
+        self.net_force = self.net_force + force
+
+    def apply_forces(self):
+        acc = self.net_force.scale(self.mass)
+        self.velocity = self.velocity.add(acc)
+        if abs(self.velocity) > self.top_speed:
+            self.velocity = normalize(self.velocity).scale(self.top_speed)
+        self.pos = self.pos.add(self.velocity)
 
     def tick(self):
-        dx = randint(-1, 1)
-        dy = randint(-1, 1)
-        self.pos = (self.pos[0] + dx, self.pos[1] + dy)
+        if self.is_at_target():
+            self.set_target()
+        self.net_force = vector.obj(x=0, y=0)
+        diff = self.target - self.pos
+        dist = abs(diff)
+        G = 30
+        force_mag = G * self.mass / (dist * dist)
+        force = normalize(diff).scale(force_mag)
+        if abs(force) < self.min_speed:
+            force = normalize(force).scale(self.min_speed)
+        drag = self.velocity.scale(-(G * self.mass / (dist * dist)))
+        self.add_force(force)
+        self.add_force(drag)
+        self.apply_forces()
 
     def draw(self, screen: pygame.Surface):
-        screen_size = (screen.get_width(), screen.get_height())
-        pos = self.pos
-        size = self.size
-        draw_pos = (pos[0] - size / 2, pos[1] - size / 2)
-        pygame.draw.ellipse(screen, self.COLOR, [draw_pos[0], draw_pos[1], size, size])
+        self.draw_circle(self.pos, self.size, self.COLOR, screen)
+        self.draw_circle(self.target, self.size.scale(0.5), self.TARGET_COLOR, screen)
+
+    def draw_circle(self, pos: VectorObject2D, size: VectorObject2D, color, screen):
+        draw_pos = pos - (size / 2)
+        pygame.draw.ellipse(screen, color, [draw_pos.x, draw_pos.y, size.x, size.y])
 
 
 class OrbPool(DrawableInterface, TickableInterface):
@@ -73,7 +131,7 @@ class Game:
 
     def add_objects(self):
         orb = Orb()
-        orb.init((self.WIDTH / 2, self.HEIGHT / 2), 10)
+        orb.init(vector.obj(x=self.WIDTH / 2, y=self.HEIGHT / 2), 10)
         self.drawables.append(orb)
         self.tickables.append(orb)
 
