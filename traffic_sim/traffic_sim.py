70a3b2d00a8aa694964efd8b5d860787e180e1c0
Josh Hendershot <hendershotjoshr@gmail.com>
2021-11-10 12:26:32 -0400

    added test tile to scene

--- traffic_sim/traffic_sim.py
+++ traffic_sim/traffic_sim.py
@@ -1,10 +1,19 @@
-from typing import List
-from orb_pool import OrbPool
+from direct.showbase.ShowBase import ShowBase
 
 
-class Game:
-    def run():
-        pass
+class Game(ShowBase):
+    def __init__(self, fStartDirect=True, windowType=None):
+        super().__init__(fStartDirect=fStartDirect, windowType=windowType)
+        self.scene = self.loader.loadModel("models/environment")
+        self.scene.reparentTo(self.render)
+        self.scene.setScale(0.25, 0.25, 0.25)
+        self.scene.setPos(-8, 42, 0)
+
+        self.tile = self.loader.loadModel("../assets/egg/city.egg")
+        self.tile.reparentTo(self.render)
+        self.tile.setScale(1, 1, 1)
+        self.tile.setPos(0, 0, 0)
+
 
 if __name__ == "__main__":
     global GAME
