d346abdb850a60cb5685bfe72d1cb6b0c33b64c6
Josh Hendershot <hendershotjoshr@gmail.com>
2021-11-11 07:20:25 -0400

    added game data class

--- traffic_sim/traffic_sim.py
+++ traffic_sim/traffic_sim.py
@@ -1,4 +1,12 @@
+from typing import NamedTuple
 import pygame
+
+
+class GameData:
+    nav_network: None  # node tree or something
+    entities: None  # array of entities (growable, active/inactive entities)
+    tiles: None  # 2d array of tiles, will almost never change
+    meta_data: None  # some game properties (speed, size, etc.), will almost never change
 
 
 class Game:
@@ -8,6 +16,5 @@ class Game:
 
 
 if __name__ == "__main__":
-    global GAME
     GAME = Game()
     GAME.run()
