402ac6637b2a0f8ccb43585e192699e1d8a50402
Josh Hendershot <hendershotjoshr@gmail.com>
2021-11-15 08:36:06 -0400

    started dev on generateFromGraph

--- src/traffic_sim/mapgenerator.py
+++ src/traffic_sim/mapgenerator.py
@@ -1,5 +1,5 @@
 from typing import List, Iterator, Tuple
-from .pathfinding import Point
+from .pathfinding import Point, PointGraph
 from dataclasses import dataclass
 
 sampleIds = [
@@ -73,17 +73,28 @@ def pointToListId(ids: List[List[int]], pos: Point) -> int:
 
 
 grass = (0, 2)
-down = (0, 0)
-right = (0, 1)
-cross = (9, 0)
-uend = (8, 2)
-dend = (9, 2)
-lend = (8, 3)
-rend = (9, 3)
-urturn = (1, 0)
-ulturn = (2, 0)
-drturn = (1, 1)
-dlturn = (2, 1)
+sand = (1, 2)
+ud = (0, 0)
+lr = (0, 1)
+udlr = (9, 0)
+d = (8, 2)
+u = (9, 2)
+r = (8, 3)
+l = (9, 3)
+dr = (1, 0)
+dl = (2, 0)
+ur = (1, 1)
+ul = (2, 1)
+udr = (4, 2)
+udl = (5, 2)
+ulr = (4, 3)
+dlr = (5, 3)
+
+bitmask = [sand, u, l, ul, r, ur, lr, ulr, d, ud, dl, udl, dr, udr, dlr, udlr]
+
+
+def generateFromGraph(graph: PointGraph):
+    pass
 
 
 def generate(ids: List[List[int]]):
@@ -112,36 +123,36 @@ def get_row_spriteids(graph: TileGraph, y: int, ids_row: List[int]):
                 and neighbors.right == 1
                 and neighbors.left == 1
             ):
-                sprites_row.append(cross)
+                sprites_row.append(udlr)
                 continue
             if neighbors.right == 1 and neighbors.left == 1:
-                sprites_row.append(right)
+                sprites_row.append(lr)
                 continue
             if neighbors.up == 1 and neighbors.down == 1:
-                sprites_row.append(down)
+                sprites_row.append(ud)
                 continue
             if neighbors.up == 1 and neighbors.right == 1:
-                sprites_row.append(drturn)
+                sprites_row.append(ur)
                 continue
             if neighbors.up == 1 and neighbors.left == 1:
-                sprites_row.append(dlturn)
+                sprites_row.append(ul)
                 continue
             if neighbors.down == 1 and neighbors.right == 1:
-                sprites_row.append(urturn)
+                sprites_row.append(dr)
                 continue
             if neighbors.down == 1 and neighbors.left == 1:
-                sprites_row.append(ulturn)
+                sprites_row.append(dl)
                 continue
             if neighbors.down == 1:
-                sprites_row.append(uend)
+                sprites_row.append(d)
                 continue
             if neighbors.up == 1:
-                sprites_row.append(dend)
+                sprites_row.append(u)
                 continue
             if neighbors.right == 1:
-                sprites_row.append(lend)
+                sprites_row.append(r)
                 continue
             if neighbors.left == 1:
-                sprites_row.append(rend)
+                sprites_row.append(l)
                 continue
     return sprites_row
--- tests/test_mapgenerator.py
+++ tests/test_mapgenerator.py
@@ -3,22 +3,36 @@ import unittest
 
 from src.traffic_sim.mapgenerator import (
     grass,
-    down,
-    right,
-    cross,
-    uend,
-    dend,
-    rend,
-    lend,
-    urturn,
-    ulturn,
-    drturn,
-    dlturn,
+    ud,
+    lr,
+    udlr,
+    d,
+    u,
+    l,
+    r,
+    dr,
+    dl,
+    ur,
+    ul,
 )
+
+n = grass
 
 
 class Test_TestMapGenerator(unittest.TestCase):
     def test_generate_1(self):
+        mapSize = (5, 5)
+        graphNodes = {
+            (0, 1): [(1, 1)],
+            (1, 0): [(1, 1), (2, 0)],
+            (1, 1): [(0, 1), (1, 0), (1, 3), (3, 1)],
+            (1, 3): [(1, 1), (3, 3)],
+            (3, 1): [(1, 1), (3, 3)],
+            (3, 3): [(1, 3), (3, 1), (3, 4), (4, 3)],
+            (3, 4): [(3, 3)],
+            (4, 0): [(1, 0), (4, 3)],
+            (4, 3): [(3, 3), (4, 0)],
+        }
         ids = [
             [0, 0, 0, 1, 0, 0, 0],
             [0, 0, 0, 1, 0, 0, 0],
@@ -29,40 +43,38 @@ class Test_TestMapGenerator(unittest.TestCase):
             [0, 0, 0, 1, 0, 0, 0],
         ]
         expectedSpriteIds = [
-            [grass, grass, grass, uend, grass, grass, grass],
-            [grass, grass, grass, down, grass, grass, grass],
-            [grass, grass, grass, down, grass, grass, grass],
-            [lend, right, right, cross, right, right, rend],
-            [grass, grass, grass, down, grass, grass, grass],
-            [grass, grass, grass, down, grass, grass, grass],
-            [grass, grass, grass, dend, grass, grass, grass],
+            [n, dr, lr, lr, lr, dl],
+            [r, udlr, lr, dl, ud],
+            [n, ud, n, ud, ud],
+            [n, ur, lr, udlr, ul],
+            [n, n, n, u, n],
         ]
 
         actualSpriteIds = mapgen.generate(ids).spriteIds
         self.assertListEqual(expectedSpriteIds, actualSpriteIds)
 
-    def test_generate_2(self):
-        ids = [
-            [0, 0, 1, 1, 0, 0, 0],
-            [0, 0, 0, 1, 0, 0, 0],
-            [0, 1, 0, 1, 0, 0, 0],
-            [0, 1, 1, 1, 1, 1, 1],
-            [0, 0, 0, 1, 0, 0, 0],
-            [0, 0, 0, 1, 0, 0, 0],
-            [0, 0, 0, 1, 0, 0, 0],
-        ]
-        expectedSpriteIds = [
-            [grass, grass, lend, ulturn, grass, grass, grass],
-            [grass, grass, grass, down, grass, grass, grass],
-            [grass, uend, grass, down, grass, grass, grass],
-            [grass, drturn, right, cross, right, right, rend],
-            [grass, grass, grass, down, grass, grass, grass],
-            [grass, grass, grass, down, grass, grass, grass],
-            [grass, grass, grass, dend, grass, grass, grass],
-        ]
+    # def test_generate_2(self):
+    #     ids = [
+    #         [0, 0, 1, 1, 0, 0, 0],
+    #         [0, 0, 0, 1, 0, 0, 0],
+    #         [0, 1, 0, 1, 0, 0, 0],
+    #         [0, 1, 1, 1, 1, 1, 1],
+    #         [0, 0, 0, 1, 0, 0, 0],
+    #         [0, 0, 0, 1, 0, 0, 0],
+    #         [0, 0, 0, 1, 0, 0, 0],
+    #     ]
+    #     expectedSpriteIds = [
+    #         [grass, grass, lend, ulturn, grass, grass, grass],
+    #         [grass, grass, grass, down, grass, grass, grass],
+    #         [grass, uend, grass, down, grass, grass, grass],
+    #         [grass, drturn, right, cross, right, right, rend],
+    #         [grass, grass, grass, down, grass, grass, grass],
+    #         [grass, grass, grass, down, grass, grass, grass],
+    #         [grass, grass, grass, dend, grass, grass, grass],
+    #     ]
 
-        actualSpriteIds = mapgen.generate(ids).spriteIds
-        self.assertListEqual(expectedSpriteIds, actualSpriteIds)
+    #     actualSpriteIds = mapgen.generate(ids).spriteIds
+    #     self.assertListEqual(expectedSpriteIds, actualSpriteIds)
 
 
 if __name__ == "__main__":
